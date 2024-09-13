36674167d05b6504159d3eaae10af8f231a2b9b5
sand-del <sandradelimoge@gmail.com>
2024-09-13 21:17:04 +0200

    Main, GamePanel

---
+++ src/main/GamePanel.java
@@ -0,0 +1,47 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	// SCREEN SETTINGS
+	final int originalTileSize = 16; // 16x16 tile
+	final int scale = 3;
+	
+	final int tileSize = originalTileSize * scale; // 48x48 tile
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 12;
+	final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+	
+	Thread gameThread;
+	
+	public GamePanel() {
+		
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black); //maybe not necessary
+		this.setDoubleBuffered(true); //for better rendering performance
+	}
+
+	public void startGameThread() {
+		
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	@Override
+	public void run() {
+		
+	}
+	
+	
+
+}
